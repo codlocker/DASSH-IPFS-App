f0bef1e14a1a2a4a4f6d94e989195384c035b40d
Daniel Liao <danielliao0426@gmail.com>
2024-02-25 06:21:49 -0700

    upload zone set

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
+import React, { useState } from "react";
 import { useStorageUpload } from "@thirdweb-dev/react";
+import { FileUploader } from "react-drag-drop-files";
 import styles from "../styles/Home.module.css";
 import { NextPage } from "next";
-import { useState } from "react";
 
 const Home: NextPage = () => {
   const [file, setFile] = useState<any>();
@@ -22,11 +23,9 @@ const Home: NextPage = () => {
       
       if(uploadURL.length > 0) {
         setUploadFileUri(uploadURL[0]);
-
         setIsUploading(false);
       }
     }
-    
   }
 
   const handleFileChange = (event: any) => {
@@ -34,20 +33,29 @@ const Home: NextPage = () => {
     setFile(selectedFile);
   };
 
+  const handleDrop = (files: FileList) => {
+    setFile(files[0]);
+  };
+
   return (
     <main className={styles.main}>
       <div className={styles.container}>
         <h1 className={styles.title}>File Upload</h1>
-        <input type="file" onChange={handleFileChange} className={styles.fielUpload}/>
+        <div className={styles.dragDropContainer}>
+          <FileUploader
+            handleChange={handleDrop}
+            name="file"
+            types={["JPG", "PNG", "GIF"]}
+            className={styles.dragDrop}
+          />
+        </div>
+        <input type="file" onChange={handleFileChange} className={styles.fileUpload} />
         <button className={styles.uploadBtn} onClick={uploadToIpfs} disabled={isUploading}>
           {isUploading ? 'Uploading...' : 'Upload'}
         </button>
         {isUploading && <div className={styles.spinner}></div>}
-
         <p className={styles.uploadUrl}>Upload URL: {uploadFileUri}</p>
-
-
-        { uploadFileUri && <img src={uploadFileUri} alt="Image"></img> }
+        {uploadFileUri && <img src={uploadFileUri} alt="Image" />}
       </div>
     </main>
   );
